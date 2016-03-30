13af19f4ed7eb5e501c40a51ebf7c9dd7faeaf3f
starwarsgood <comp822012016@mail.ru>
2016-03-30 16:14:11 +0300

    JobJS

---
+++ main.js
@@ -0,0 +1,14 @@
+(function() {
+var div = document.getElementsByClassName('test');
+ style = div[0].style;
+
+ style.color = "red";
+ style.backgroundColor = "Green";
+ style.border = "5px solid blue";
+ style.padding = "10px";
+ style.fontSize = "100px";
+ style.fontFamily = "Tahoma";
+
+
+
+})();
